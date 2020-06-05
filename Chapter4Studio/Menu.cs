0a55093b933566a7b74980a7d348c6b34d314c74
devhl-labs <shawnkanyer@gmail.com>
2020-06-05 13:05:24 -0400

    completed studio

---
+++ Chapter4Studio/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Chapter4Studio
+{
+    public class Menu
+    {
+        private Dictionary<string, MenuItem> MenuItems { get; } = new Dictionary<string, MenuItem>();
+
+        public DateTime CreatedAt { get; }
+
+        public DateTime ModifiedAt { get; }
+
+        public string Name { get; }
+
+        public Menu(string name, DateTime createdAt, DateTime modifiedAt)
+        {
+            Name = name;
+
+            CreatedAt = createdAt;
+
+            ModifiedAt = modifiedAt;
+        }
+
+        public bool AddItem(MenuItem menuItem) => MenuItems.TryAdd(menuItem.Name, menuItem);
+
+        public ImmutableList<MenuItem> GetItems => MenuItems.Values.ToImmutableList();
+    }
+}
---
+++ Chapter4Studio/MenuItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Chapter4Studio
+{
+    public enum MenuCategory
+    {
+        None,
+        Appetizer,
+        MainCourse,
+        Dessert
+    }
+
+    public class MenuItem
+    {
+        public string Name { get; }
+
+        public double Price { get; }
+
+        public string Description { get; } = string.Empty;
+
+        public MenuCategory MenuCategory { get; }
+
+        public DateTime CreatedAt { get; }
+
+        public MenuItem(string name, double price, string description, MenuCategory menuCategory, DateTime createdAt)
+        {
+            Name = name;
+
+            Price = price;
+
+            Description = description;
+
+            MenuCategory = menuCategory;
+
+            CreatedAt = createdAt;
+        }
+    }
+}
